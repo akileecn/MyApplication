e2738a156b5ab7017d3a5311089ac422ffb977de
akileecn <121107209@qq.com>
2015-12-10 19:32:49 +0800

    Create OutgoingCallReceiver.java

---
+++ 手机安全卫士/src/main/java/cn/aki/mobilesafe/receiver/OutgoingCallReceiver.java
@@ -0,0 +1,21 @@
+package cn.aki.mobilesafe.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import cn.aki.mobilesafe.db.dao.AddressDao;
+
+/**
+ * Created by Administrator on 2015/12/4.
+ * 去电监听
+ */
+public class OutgoingCallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String number=getResultData();
+        String address=AddressDao.getAddress(number);
+        Toast.makeText(context, address, Toast.LENGTH_SHORT).show();
+    }
+}
